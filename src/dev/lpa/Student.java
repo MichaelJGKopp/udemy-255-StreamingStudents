634c2714024e0e0e79e3947e9570644c15948195
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-26 20:22:54 +0200

    setup class Student

--- src/dev/lpa/Student.java
+++ src/dev/lpa/Student.java
@@ -5,23 +5,29 @@ import java.util.*;
 
 public class Student {
 
-  private long studentId;
-  private String countryCode;
-  private int yearEnrolled;
-  private int ageEnrolled;
-  private String gender;
-  private boolean programmingExperience;
-  private Map<String, CourseEngagement> engagementMap;
+  private static long lastStudentId = 1;
+  private final static Random random = new Random();
 
-  public Student(long studentId, String countryCode, int yearEnrolled, int ageEnrolled,
-                 String gender, boolean programmingExperience) {
-    this.studentId = studentId;
+  private final long studentId;
+  private final String countryCode;
+  private final int yearEnrolled;
+  private final int ageEnrolled;
+  private final String gender;
+  private final boolean programmingExperience;
+  private final Map<String, CourseEngagement> engagementMap = new HashMap<>();
+
+  public Student(String countryCode, int yearEnrolled, int ageEnrolled, String gender,
+                 boolean programmingExperience, Course... courses) {
+    this.studentId = lastStudentId++;
     this.countryCode = countryCode;
     this.yearEnrolled = yearEnrolled;
     this.ageEnrolled = ageEnrolled;
     this.gender = gender;
     this.programmingExperience = programmingExperience;
-    this.engagementMap = new TreeMap<>();
+
+    for (Course course : courses) {
+      addCourse(course, LocalDate.of(yearEnrolled, 1, 1));
+    }
   }
 
   public long getStudentId() {
@@ -44,12 +50,12 @@ public class Student {
     return gender;
   }
 
-  public boolean isProgrammingExperience() {
+  public boolean hasProgrammingExperience() {
     return programmingExperience;
   }
 
   public Map<String, CourseEngagement> getEngagementMap() {
-    return new TreeMap<>(engagementMap);
+    return Map.copyOf(engagementMap);
   }
 
   public int getYearsSinceEnrolled() {
@@ -69,12 +75,14 @@ public class Student {
 
   public void addCourse(Course course, LocalDate enrollDate) {
 
-    engagementMap.put(course.getCourseCode(), new CourseEngagement(course, enrollDate));
+    engagementMap.put(course.getCourseCode(), new CourseEngagement(course, enrollDate,
+      "Enrollment"));
   }
 
   public int getMonthsSinceActive(String courseCode) {
 
-    return engagementMap.get(courseCode).getMonthsSinceActive();
+    CourseEngagement info = engagementMap.get(courseCode);
+    return info == null ? 0 : info.getMonthsSinceActive();
   }
 
   public int getMonthsSinceActive() {
@@ -93,7 +101,28 @@ public class Student {
 
   public double getPercentComplete(String courseCode) {
 
-    return engagementMap.get(courseCode).getPercentComplete();
+    CourseEngagement info = engagementMap.get(courseCode);
+    return courseCode == null ? 0 : info.getPercentComplete();
   }
 
+  public void watchLecture(String courseCode, int lectureNumber, int month, int year) {
+
+    var activity = engagementMap.get(courseCode);
+    if (activity != null) {
+      activity.watchLecture(lectureNumber, LocalDate.of(year, month, 1));
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+      "studentId=" + studentId +
+      ", countryCode='" + countryCode + '\'' +
+      ", yearEnrolled=" + yearEnrolled +
+      ", ageEnrolled=" + ageEnrolled +
+      ", gender='" + gender + '\'' +
+      ", programmingExperience=" + programmingExperience +
+      ", engagementMap=" + engagementMap +
+      '}';
+  }
 }
