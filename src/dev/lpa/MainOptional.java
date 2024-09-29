53769482a73bc30d7d814f08998c9bb8e6bd5ec8
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-29 12:08:00 +0200

    replace Optional.of with ofNullable

--- src/dev/lpa/MainOptional.java
+++ src/dev/lpa/MainOptional.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -17,19 +18,25 @@ public class MainOptional {
       .limit(1000)
       .collect(Collectors.toList());  // mutable
 
-    Optional<Student> o1 = getStudent(null, "first");
+    Optional<Student> o1 = getStudent(new ArrayList<>(), "first");
     System.out.println("Empty = " + o1.isEmpty() + ", Present = " + o1.isPresent());
     System.out.println(o1);
+
+    students.add(0, null);
+    Optional<Student> o2 = getStudent(students, "first");
+    System.out.println("Empty = " + o2.isEmpty() + ", Present = " + o2.isPresent());
+    System.out.println(o2);
+
   }
   private static Optional<Student> getStudent(List<Student> list, String type) {
 
     if (list == null || list.isEmpty()) {
       return Optional.empty();  // never return null when returning type Optional
     } else if (type.equals("first")) {
-      return Optional.of(list.get(0));
+      return Optional.ofNullable(list.get(0));
     } else if (type.equals("last")) {
-      return Optional.of(list.get(list.size() - 1));
+      return Optional.ofNullable(list.get(list.size() - 1));
     }
-    return Optional.of(list.get(new Random().nextInt(list.size())));
+    return Optional.ofNullable(list.get(new Random().nextInt(list.size())));
   }
 }
