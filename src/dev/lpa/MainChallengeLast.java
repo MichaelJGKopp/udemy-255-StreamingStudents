ca3f26386f94d467cab0d1376634ad6171a14678
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-30 13:46:32 +0200

    make final corrections before solution

--- src/dev/lpa/MainChallengeLast.java
+++ src/dev/lpa/MainChallengeLast.java
@@ -1,8 +1,11 @@
 package dev.lpa;
 
 import java.time.LocalDate;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 public class MainChallengeLast {
 
@@ -19,12 +22,66 @@ public class MainChallengeLast {
       .toList();
 
     students.stream()
-      .limit(5)
-      .forEach(System.out::println);
+      .limit(10)
+//      .peek(System.out::println)
+      .forEach(s -> System.out.print(s.getPercentComplete("PYMC") + " "));
+    System.out.println();
+
+    System.out.println("Courses average percentage completed");
+    String[] courseCodes = {"PYMC", "JMC", "CGIJ"};
+    for (var cc : courseCodes) {
+      System.out.printf("%s %.2f%% %n", cc, students.stream()
+          .mapToDouble(s -> s.getPercentComplete(cc))
+          .filter(d -> d != 0.0)
+          .average()
+          .orElseGet(() -> 0.0)
+      );
+    }
+    System.out.println();
+
+    for (var cc : courseCodes) {
+      var byClass = students.stream()
+        .filter(s -> s.getEngagementMap().values().stream()
+          .map(ce -> ce.getCourseCode().equals(cc))
+          .findAny()
+          .orElse(false)
+        )
+        .toList();
+      System.out.println("Class: " + cc + " Student Count: " + byClass.size());
+    }
 
     var byCount = students.stream()
-        .collect(Collectors.groupingBy((Student s) -> (long) s.getEngagementMap().values().size()));
-
+      .collect(groupingBy((Student s) -> (long) s.getEngagementMap().values().size()));
     byCount.forEach((k, v) -> System.out.println("Lectures: " + k + " Student Count: " + v.size()));
-  }
+
+      var byCount2 = students.stream()
+        .collect(groupingBy((Student s) -> (long) s.getEngagementMap().values().size(),
+          Collectors.averagingDouble(s -> {
+            double sum = 0;
+            int divisor = 0;
+            for (var cc : courseCodes) {
+              double percentage = s.getPercentComplete(cc);
+              if (percentage != 0)
+                divisor++;
+              sum += percentage;
+            }
+            return sum / divisor;
+          })));
+      byCount2.forEach((k, v) -> System.out.printf(
+        "Lecture Count: %d Student percent complete: %.2f%% %n", k, v));
+    System.out.println();
+
+      for (var cc : courseCodes) {
+        var map = students.stream()
+          .flatMap(s -> s.getEngagementMap().values()
+            .stream()
+            .filter(ce -> ce.getCourseCode().equals(cc))
+            .map(ce -> ce.getLastActivityYear())
+          )
+          .collect(groupingBy(s -> s, TreeMap::new, counting()));
+        System.out.println("Course Code: " + cc);
+        map.forEach((k, v) -> System.out.printf("Year: %d Activity Count: %d %n", k, v));
+        System.out.println();
+      }
+    }
 }
